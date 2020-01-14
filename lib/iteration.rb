97f4a600d745ef6a4e9e9f6e95dc87951c34980d
Matt Milton <mattmilton57@gmail.com>
2020-01-14 03:20:38 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -17,10 +17,10 @@ row_index = 0
 while row_index < src.count do
   while src[row_index][0] > src[row_index][1] do
     biggie_smalls= src[row_index][0]
-    row_index_index +=1
-  while src[row_index][1] > src[row_index][0] do
+    row_index +=1
+  else
     biggie_smalls= src[row_index][1] 
-    row_index_index +=1
+    row_index +=1
   end
 biggie_smalls
 end 
